161ee0cd46b1d46ac0561908f5c037a74c3c2fca
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-09-03 18:27:22 +0530

    Time: 27 ms (43.76%), Space: 83.7 MB (57.88%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -23,23 +23,23 @@ public:
             largestArea = max(largestArea , (1LL)*(n - t.first)*t.second);
             stk.pop();
         }
-//from right to left
-        for(int i = n-1;i>=0;i--){
-            int newIdx = i;
-            while(!stk.empty()){
-                pair<int,int> t = stk.top();
-                if(t.second <= heights[i])break;
-                largestArea = max(largestArea , (1LL)*(t.first - i)*t.second);
-                newIdx = t.first;
-                stk.pop();
-            }
-            stk.push({newIdx,heights[i]});
-        }
-        while(!stk.empty()){
-            pair<int,int> t = stk.top();
-            largestArea = max(largestArea , (1LL)*(t.first + 1)*t.second);
-            stk.pop();
-        }
+// //from right to left
+//         for(int i = n-1;i>=0;i--){
+//             int newIdx = i;
+//             while(!stk.empty()){
+//                 pair<int,int> t = stk.top();
+//                 if(t.second <= heights[i])break;
+//                 largestArea = max(largestArea , (1LL)*(t.first - i)*t.second);
+//                 newIdx = t.first;
+//                 stk.pop();
+//             }
+//             stk.push({newIdx,heights[i]});
+//         }
+//         while(!stk.empty()){
+//             pair<int,int> t = stk.top();
+//             largestArea = max(largestArea , (1LL)*(t.first + 1)*t.second);
+//             stk.pop();
+//         }
     return largestArea;
     }
 };
