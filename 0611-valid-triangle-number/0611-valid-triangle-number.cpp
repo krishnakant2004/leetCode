a042fbd242c984dfc66c51330b6920fd9c2ccf79
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-09-26 18:08:37 +0530

    Time: 191 ms (14.04%), Space: 16.5 MB (95.74%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -7,16 +7,22 @@ public:
 
         for(int i = 0;i<n-2;i++){
             for(int j = i+1;j<n-1;j++){
-
-                for(int k = n-1;k >= j+1;k--){
-                    if(nums[k] < (nums[i]+nums[j])){
-                        count += (k - j);
-                        break;
-                    }
-                }
+                int idx = getIdx(nums,j+1,n-1,nums[i]+nums[j]);
+                count += (idx - j);
             }
         }
         return count;
+    }
+
+    int getIdx(vector<int>& vec,int l,int r,int sum){
+        if(l > r) return r;
+
+        int mid = l + (r-l)/2;
+        if(vec[mid] < sum){
+            return getIdx(vec,mid+1,r,sum);
+        }else{
+            return getIdx(vec,l,mid-1,sum);
+        }
     }
 
     
