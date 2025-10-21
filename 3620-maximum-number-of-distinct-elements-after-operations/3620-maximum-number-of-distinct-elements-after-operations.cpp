a93850cf689985f5b14f1352abbbdca3b97bab18
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-10-21 20:06:59 +0530

    Time: 112 ms (81.07%), Space: 104.9 MB (14.09%) - LeetHub

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        sort(nums.begin(),nums.end(),greater<>());
+
+        //store val freq
+        vector<pair<int,int>> pr;
+        for(int i = 0;i<n;i++){
+            if(i-1 >= 0 && nums[i-1] == nums[i]){
+                pr[pr.size()-1].second++;
+            }else{
+                pr.push_back({nums[i] , 1});
+            }
+        }
+        
+        int len = 0;
+        int Limit = nums[0] + k;
+        for(int i = 0; i<pr.size(); i++){
+            int uLimit = pr[i].first + k;
+            int lLimit = pr[i].first - k;
+            int r = (uLimit > Limit) ? Limit : uLimit;
+            int l = ((r - pr[i].second) >= lLimit-1) ? (r - pr[i].second) : (lLimit-1);
+            Limit = l;
+
+            len += (r - l) > 0 ? (r-l) : 0;
+        }
+        return len;
+    }
+};
