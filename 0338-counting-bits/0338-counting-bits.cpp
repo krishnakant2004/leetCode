176424ac2dc005901507eceb4c64da347fa8a011
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-12-17 01:03:45 +0530

    Time: 0 ms (100%), Space: 10.7 MB (55.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        unordered_map<int,int> mp;
         vector<int> res(n+1,0);
         res[0] = 0;
-        mp[0] = 0;
         for(int i = 1;i<=n;i++){
-            int val = mp[i>>1] + (i & 1);
-            mp[i] = val;
-            res[i] = val; 
+            res[i] = res[i>>1] + (i & 1); 
         }
         return res;
     }
