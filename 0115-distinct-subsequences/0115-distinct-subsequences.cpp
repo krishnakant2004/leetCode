b91768e0f0633779d03d533397c4495def1b060a
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-12-24 15:10:40 +0530

    Time: 29 ms (58.29%), Space: 44.2 MB (32.25%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
+    const long long LIMIT = INT_MAX;
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return getDist(s,t,n,m,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        // return getDist(s,t,n,m,dp);
+
+        //bottom up
+          vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if (s[i-1] == t[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    if (dp[i][j] > LIMIT) dp[i][j] = LIMIT;
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
     }
 
     int getDist(string& s,string& t,int n,int m,vector<vector<int>> &dp){
