c585b45f50f56dcfbe2c6ba2b0f5e83106d9cfc0
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-12-26 20:45:21 +0530

    Time: 534 ms (30.96%), Space: 159.1 MB (20.06%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);       
+
         int n = nums.size();
         vector<unordered_map<long long,int>> dp(n);
 
-        long long ans = 0;
+        int ans = 0;
         for(int i = 1;i<n;i++){
             for(int j = 0;j<i;j++){
                 long long diff = (long long)nums[i] - nums[j];
-                long long cnt = dp[j].count(diff) ? dp[j][diff] : 0;
+                int cnt = dp[j].count(diff) ? dp[j][diff] : 0;
               
                 //update
                 ans += cnt;
                 dp[i][diff] += 1 + cnt;
             }
         }
-        return (int)ans;
+        return ans;
     }
 };
