9a88f9e716e6f9e3ed57a43c094270473fd305f8
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2025-12-27 22:27:12 +0530

    Time: 0 ms (100%), Space: 9.9 MB (59.52%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res = {{}};
+    // vector<vector<int>> subsets(vector<int>& nums) {
+    //     vector<vector<int>> res = {{}};
         
-        for(int i = 0;i<nums.size();i++){
-            vector<vector<int>> vec = res;
-            for(auto v : res){
-                v.push_back(nums[i]);
-                vec.push_back(v);
+    //     for(int i = 0;i<nums.size();i++){
+    //         vector<vector<int>> vec = res;
+    //         for(auto v : res){
+    //             v.push_back(nums[i]);
+    //             vec.push_back(v);
+    //         }
+    //         res = vec;
+    //     }
+    //     return res;
+    // }
+
+    //using bit manupulation
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int total = 1 << n;   // 2^n subsets
+
+        vector<vector<int>> ans;
+
+        for(int mask = 0; mask < total; mask++) {
+            vector<int> subset;
+
+            for(int i = 0; i < n; i++) {
+                if(mask & (1 << i)) {
+                    subset.push_back(nums[i]);
+                }
             }
-            res = vec;
+
+            ans.push_back(subset);
         }
-        return res;
+
+        return ans;
     }
+
+
 };
