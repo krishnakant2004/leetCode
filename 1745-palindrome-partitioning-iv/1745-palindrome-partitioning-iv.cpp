e0be27b5497de14dad32325a501cdd2852f5aa30
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2026-01-07 12:49:31 +0530

    Time: 221 ms (41.57%), Space: 201.4 MB (10.7%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkPartitioning(string s) {
+        int n = s.size();
+
+        vector<vector<int>> pl(n,vector<int>(n,0));
+
+        for(int i = 0;i<n;i++) pl[i][i] = 1;
+
+        for(int len = 2;len <= n;len++){
+            for(int i = 0;i<n-len+1;i++){
+                int j = i+ len -1;
+                if (s[i] == s[j]) {
+                    if (len == 2) 
+                        pl[i][j] = 1;
+                    else 
+                        pl[i][j] = pl[i+1][j-1];
+                } else {
+                    pl[i][j] = 0;
+                }
+            }
+        }
+
+        vector<int> dp(n,0);
+        int limit = 3;
+        //for 1
+        for(int i = 0;i<n;i++) dp[i] = pl[0][i];
+
+        for(int k = 2;k<=limit;k++){
+            vector<int> ndp(n,0);
+            for(int i = 0;i<n;i++){
+                for(int j = k-2;j<i;j++){
+                    if(dp[j] != 0 && pl[j+1][i]){
+                        ndp[i] = 1;
+                        break;
+                    }
+                }
+            }
+            swap(ndp,dp);
+        }
+        return dp[n-1];
+    }
+};
