6e923e34c3935b1b20f8af9e4ba4385007b1a958
Krishnakant dinkar <145291054+krishnakant2004@users.noreply.github.com>
2026-01-07 14:13:36 +0530

    Time: 264 ms (25.86%), Space: 253.7 MB (10.92%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        vector<vector<int>> pl(n,vector<int>(n+1,0));
+
+        for(int i = 0;i<n;i++) pl[i][i] = 1;
+        //palindrom
+
+        for(int len = 2;len <= n;len++){
+            for(int i = 0;i<n-len+1;i++){
+                int j = i+ len -1;
+                if (s[i] == s[j]) {
+                    if (len == 2) 
+                        pl[i][j] = 1;
+                    else 
+                        pl[i][j] = pl[i+1][j-1];
+                } else {
+                    pl[i][j] = 0;
+                }
+            }
+        }
+
+        vector<int> dp(n+1,0);
+        for(int i = k;i<=n;i++){
+            dp[i] = dp[i-1];
+            for(int j = i-k+1;j>0;j--){
+                if(pl[j-1][i-1]){
+                    dp[i] = max(dp[i] , 1 + dp[j-1]);
+                    break;
+                }
+            }
+        }
+        return dp[n];
+
+
+    }
+};
